540327e735090602e1cd5dc6eb79534863961d2c
Ryan Cleven <rcleven@mac.com>
2023-01-16 11:01:20 -0800

    Updated to new interface
    Trying to get closer to vanilla heaps

--- lib/hxd/impl/WindowDriver.hx
+++ lib/hxd/impl/WindowDriver.hx
@@ -1,20 +1,69 @@
 package hxd.impl;
+import hxd.impl.MouseMode;
+import hxd.Window.DisplaySetting;
+import hxd.Window.Monitor;
 
-typedef WindowHandle = sdl.Window;
+@:forward
+#if heaps_forge
+abstract WindowHandle(sdl.WindowForge) from sdl.WindowForge to sdl.WindowForge{
+#else
+abstract WindowHandle(sdl.Window) {
+#end
+	public function getCurrentMonitor() {
+		return this.currentMonitor;
+	}
+
+	public function setCursorPosition(mode : MouseMode, x: Int, y : Int) {
+        if (mode == MouseMode.Absolute) {
+            this.warpMouse(x, y);
+        }
+    }
+
+    
+	public function setMouseMode(mode : MouseMode) {
+    }
+
+
+	public function getMouseClip() {
+        return this.grab;
+    }
+
+	public function setMouseClip(clip:Bool) : Bool{
+        this.grab = clip;
+        return clip;
+    }
+
+
+}
+
 typedef DisplayMode = sdl.Window.DisplayMode;
+typedef Event = sdl.Event;
 
 class WindowDriver {
-    public static function createWindow(heapsWindow : hxd.Window, title:String, width:Int, height:Int, fixed:Bool = false) : WindowHandle{
-        var sdlFlags = if (!fixed) sdl.Window.SDL_WINDOW_SHOWN | sdl.Window.SDL_WINDOW_RESIZABLE else sdl.Window.SDL_WINDOW_SHOWN;
-        #if heaps_forge
-        var window = new sdl.WindowForge(title, width, height, sdl.Window.SDL_WINDOWPOS_CENTERED, sdl.Window.SDL_WINDOWPOS_CENTERED, sdlFlags);	
-        #else
-        var window = new sdl.WindowGL(title, width, height, sdl.Window.SDL_WINDOWPOS_CENTERED, sdl.Window.SDL_WINDOWPOS_CENTERED, sdlFlags);
-        #end
-        @:privateAccess heapsWindow.window = window;
-        @:privateAccess heapsWindow.windowWidth = window.width;
-        @:privateAccess heapsWindow.windowHeight = window.height;
+	public static function createWindow(heapsWindow:hxd.Window, title:String, width:Int, height:Int, fixed:Bool = false):WindowHandle {
+		var sdlFlags = if (!fixed) sdl.Window.SDL_WINDOW_SHOWN | sdl.Window.SDL_WINDOW_RESIZABLE else sdl.Window.SDL_WINDOW_SHOWN;
+		#if heaps_forge
+		var window:WindowHandle = new sdl.WindowForge(title, width, height, sdl.Window.SDL_WINDOWPOS_CENTERED, sdl.Window.SDL_WINDOWPOS_CENTERED, sdlFlags);
+		#else
+		var window:WindowHandle = new sdl.WindowGL(title, width, height, sdl.Window.SDL_WINDOWPOS_CENTERED, sdl.Window.SDL_WINDOWPOS_CENTERED, sdlFlags);
+		#end
+		@:privateAccess heapsWindow.window = window;
+		@:privateAccess heapsWindow.windowWidth = window.width;
+		@:privateAccess heapsWindow.windowHeight = window.height;
 
-        return window;
+		return window;
+	}
+
+	public static function getMonitors() {
+		return sdl.Sdl.getDisplays();
+	}
+
+    public static function getCurrentDisplaySetting(?mid : Int, mname : String, registry : Bool) : DisplaySetting {
+		return sdl.Sdl.getCurrentDisplayMode(mid == null ? 0 : mid, true);
+	}
+
+    public static function getDisplaySettings(mid : Int,  mname : String) : Array<DisplaySetting> {
+        return sdl.Sdl.getDisplayModes(mid );
     }
+
 }
